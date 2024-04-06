f989f003243a56278055a8f940be694bd0177d8d
mell-o-tron <l.pace4@studenti.unipi.it>
2024-04-06 16:32:27 +0200

    started writing compiler

--- Bootloader part/stage_2.asm
+++ Bootloader part/stage_2.asm
@@ -1,32 +1,124 @@
+; NOTE
+; We use three stacks:
+;  - the builtin stack
+;  - the operand stack
+;  - the environment stack
+;
+; There is only one environment because we use dynamic scoping, for simplicity.
+; 
+; Here is a sketch of the memory map:
+; 
+; 0x0      ...   0x4FF		unusable in real mode
+; 0x500    ...   0x7DFF		memory we can use for the stacks (including overwriting bootsector)
+; 0x7e00   ...   0x7FFFF	second stage bootloader
+;
+; We divide the stack memory in three parts, each of length 0x2855
+;   - [0x500,  0x2D54]
+; 	- [0x2D55, 0x55A9]
+;	- [0x55AA, 0x7DFF]
+
 [org 0x7e00]
 [bits 16]
 
-	; STACK SETUP
-	; NOTE:
-	; 	I made this separate from the one in stage 1, so that we can change the stack pointer more easily
-	;	when writing the code for the environment stack
+%define ENVIRONMENT_POINTER ecx
+%define OPERAND_POINTER edx
+
+%macro 	push_operand 1
+		mov ax, %1
+		mov [OPERAND_POINTER], ax
+		add OPERAND_POINTER, 2
+%endmacro
+
+%macro 	push_env 1
+		mov ax, %1
+		mov [ENVIRONMENT_POINTER], ax
+		add ENVIRONMENT_POINTER, 2
+%endmacro
+
+%macro 	pop_operand 1
+		sub OPERAND_POINTER, 2
+		mov %1, [OPERAND_POINTER]
+%endmacro
+
+%macro 	pop_env 1
+		sub ENVIRONMENT_POINTER, 2
+		mov %1, [ENVIRONMENT_POINTER]
+%endmacro
+
+
 
 	cli
 	mov ax, 0x00
 	mov ds, ax
 	mov es, ax
 	mov ss, ax
-	mov sp, 0x7C00		; stack pointer
+	mov sp, 0x2D55		; stack pointer
 	sti
 
-	; TODO setup environment stack
-	; idea: use some register that we won't use (like DX) to hold the pointer to the top of the environment stack
+	; setup stacks
+	
+	mov ENVIRONMENT_POINTER, 0x2D55
+	mov OPERAND_POINTER, 0x55AA
+	
 
 mov bx, STAGING
 call print_string
 
 
 	; GENERATED CODE WILL BE WRITTEN HERE
+	
+	; example: (lambda ((lambda (L0 + L1)) 5)) 6
 
+	; APPLY (e1 e2)
+	
+	push_operand fun_1		; compile e1
+	push_operand 6			; comple e2
+	
+	pop_operand ax			; APPLY
+	push_env ax
+	
+	pop_operand ax
+	call ax
+	
+	; test print
+	
+	pop_operand bx
+	call print_dec
+	
+	jmp $
+	
+	
+	; fundef part
+	
+	fun_1:
+		; APPLY (e1 e2)
+		push_operand fun_2	; compile e1
+		push_operand 5		; compile e2
+		
+		pop_operand ax
+		push_env ax
+		
+		pop_operand ax
+		call ax
+		
+		ret
+	
+	fun_2:
+		; e1 + e2 
+		push_operand [ENVIRONMENT_POINTER - 2]		; compile e1
+		push_operand [ENVIRONMENT_POINTER - 4]		; compile e2
+		
+		pop_operand ax		; +
+		pop_operand bx
+		add ax, bx
+		
+		push_operand ax		; return value
+		
+		ret
 
-jmp $
 
 %include"./utils/print_string.asm"
+%include"./utils/print_dec.asm"
 
 STAGING:
 	db "Separation Confirmed.", 0x0A, 0x0D, 0x00
--- Lambda part/compiler.ml
+++ Lambda part/compiler.ml
@@ -15,20 +15,49 @@ and bexp =
   | BUnop  of bunop * exp
   | Compare of comparator * exp * exp
 and exp =
-  | Lambda      : exp
-  | Apply       : exp * exp
-  | IfThenElse  : exp * exp * exp
-  | Aexp        : aexp
-  | Bexp        : bexp
-  | Var         : nat
-  | Val         : value
-with value =
-  | Closure     : environment * exp
-  | BoolConst   : bool
-  | IntConst    : Z
-with environment = Empty | Env of (value * environment)
+  | Lambda      of exp
+  | Apply       of exp * exp
+  | IfThenElse  of exp * exp * exp
+  | Aexp        of aexp
+  | Bexp        of bexp
+  | Var         of int
+  | Val         of value
+and value =
+  | Closure     of exp
+  | BoolConst   of bool
+  | IntConst    of int
 
 
 
+let function_counter = ref 0
 
+let fresh_name () =
+  function_counter := !function_counter + 1;
+  "fun_" ^ string_of_int !function_counter
+  
 
+let rec compile e = (match e with 
+  | Val v -> (match v with
+      | Closure e1   ->  let c = compile(e1) in let funname = fresh_name() in ("push_operand "^funname^"\n", funname ^":\n"^ (fst c) ^ "ret\n"^ (snd c))
+      | BoolConst b -> ("push_operand " ^ (if b then "1" else "0") ^ "\n", "")
+      | IntConst i  -> ("push_operand " ^ (string_of_int i) ^ "\n", "")
+    )
+  | Var i -> ("push_operand [ENVIRONMENT_POINTER - " ^ string_of_int ((i + 1) * 2) ^ "]\n", "")
+  | Apply (e1, e2) -> let c1 = compile(e1) in let c2 = compile(e2) in
+      ((fst c1) ^ (fst c2) ^ "pop_operand ax\npush_env ax\npop_operand ax\ncall ax\n", (snd c1) ^ (snd c2))
+    
+  | Aexp a -> (match a with 
+    | ABinop (o, e1, e2) -> (match o with
+      | Plus -> let c1 = compile(e1) in let c2 = compile(e2) in
+      ((fst c1) ^ (fst c2) ^ "pop_operand ax\npop_operand bx\nadd ax, bx\npush_operand ax\n", (snd c1) ^ (snd c2))
+    )
+    | AUnop (o, e1) -> (match o with
+      | Neg -> let c1 = compile(e1) in
+      ((fst c1) ^ "pop_operand ax\nneg ax\npush_operand ax\n", (snd c1))
+    )
+  )
+  | _ -> failwith "not implemented"
+);;
+
+let s = (compile (Apply(Val (Closure (Var 0)), Val(IntConst 10)))) in
+print_string (fst s) ; print_string ("\n; functions\n" ^(snd s))
