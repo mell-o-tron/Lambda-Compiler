a2c7f3c50fafc1c43af7e3b3fd0db599722c0b80
Alessandro Scala <alessandro.scala8@gmail.com>
2024-04-10 00:15:55 +0200

    QoL changes in lexer

--- Lambda part/lib/lexer.ml
+++ Lambda part/lib/lexer.ml
@@ -5,32 +5,42 @@ let number = [%sedlex.regexp? Plus digit]
 
 let rec token lexbuf =
     match%sedlex lexbuf with
-        | "λ."   -> (Lambda)
-        | "("    -> (LParens)
-        | ")"    -> (RParens)
-        | "+"    -> (Plus)
-        | "-"    -> (Minus)
-        | "×"    -> (Times)
-        | "¬"    -> (Not)
-        | "∧"    -> (And)
-        | "∨"    -> (Or)
-        | "="    -> (Equals)
-        | "≠"    -> (Neq)
-        | "≥"    -> (Geq)
-        | "≤"    -> (Leq)
-        | "L"    -> (LIndex)
-        | "G"    -> (GIndex)
-        | "if"   -> (If)
-        | "then" -> (Then)
-        | "else" -> (Else)
-        | white_space -> (token lexbuf)
-        | number -> (
+        | "λ." | "\\lambda."  -> (Lambda)
+        | "("                 -> (LParens)
+        | ")"                 -> (RParens)
+        | "+"                 -> (Plus)
+        | "-"                 -> (Minus)
+        | "×"  | "*"          -> (Times)
+        | "¬"  | "!"          -> (Not)
+        | "∧"  | "&"          -> (And)
+        | "∨"  | "|"          -> (Or)
+        | "="                 -> (Equals)
+        | "≠"  | "!="         -> (Neq)
+        | "≥"  | ">="         -> (Geq)
+        | "≤"  | "<="         -> (Leq)
+        | "L"                 -> (LIndex)
+        | "G"                 -> (GIndex)
+        | "if"                -> (If)
+        | "then"              -> (Then)
+        | "else"              -> (Else)
+        | white_space         -> (token lexbuf)
+        | number              -> (
             Number (int_of_string (Sedlexing.Latin1.lexeme lexbuf))
         )
-        | eof -> (EOF)
-        | any -> (
+        | "**"                -> (comment lexbuf)
+        | eof                 -> (EOF)
+        | any                 -> (
             failwith  (Printf.sprintf "Unrecognised character: \'%s\'" (Sedlexing.Latin1.lexeme lexbuf))
         )
-        | _ -> (failwith "Impossible!")
+        | _                   -> (failwith "Impossible!")
+
+and comment lexbuf =
+    match%sedlex lexbuf with
+        | "**"                -> (token lexbuf)
+        | "\n"                -> (token lexbuf)
+        | eof                 -> (EOF)
+        | any                 -> (comment lexbuf)
+        | _                   -> (failwith "Impossible!")
+
 let tokenize (lexbuf: Sedlexing.lexbuf) =
     token lexbuf
