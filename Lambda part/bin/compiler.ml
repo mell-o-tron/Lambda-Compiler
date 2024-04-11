acd2e269e56e8ed00e25045934cb6a7fea2fa756
mell-o-tron <l.pace4@studenti.unipi.it>
2024-04-11 12:33:35 +0200

    testing environment

--- Lambda part/bin/compiler.ml
+++ Lambda part/bin/compiler.ml
@@ -13,12 +13,12 @@ let fresh_jmp () =
   jmp_counter := !jmp_counter + 1;
   "branch_" ^ string_of_int !jmp_counter
   
-  
+
 (* returns a pair of strings (s1, s2) where s1 is the code of the main expression, and s2 is the code of the generated functions *)
 
 let rec compile (e: exp) = (match e with
   | Lambda e1   ->  (
-    let c = compile(e1) in let funname = fresh_name() in ("push_operand "^funname^"\n", funname ^":\n"^ (fst c) ^ "pop_env ax\nret\n\n"^ (snd c))
+    let c = compile(e1) in let funname = fresh_name() in ("push_operand "^funname^"\n", funname ^":\n"^ (fst c) ^ "ret\n\n"^ (snd c))
   )
   (* gets operand from the environment stack, and pushes it to the operand stack *)
   | Var i -> (
@@ -30,7 +30,7 @@ let rec compile (e: exp) = (match e with
   (* in order to apply e1 to e2, first compile them, then pop the operands, push the parameter to the environment stack and call the retrieved function *)
   (* add any functions created by e1 and e2 to the function zone *)
   | Apply (e1, e2) -> let c1 = compile(e1) in let c2 = compile(e2) in
-      ((fst c1) ^ (fst c2) ^ "pop_operand ax\npush_env ax\npop_operand ax\ncall ax\n", (snd c1) ^ (snd c2))
+      ((fst c1) ^ (fst c2) ^ "pop_operand ax\npush_env ax\npop_operand ax\ncall ax\npop_env ax\n", (snd c1) ^ (snd c2))
   
   | Aexp a -> (compile_aexp a)
   | Bexp b -> (compile_bexp b)
